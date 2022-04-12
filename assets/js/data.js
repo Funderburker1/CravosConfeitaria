a5935430b293da118236e4c0f0dfd38b8e7b7432
João Vitor Camargo <66924117+devjoaocamargo@users.noreply.github.com>
2022-04-12 02:06:33 -0300

    opa

--- assets/js/data.js
+++ assets/js/data.js
@@ -48,4 +48,170 @@ cookies = [
         description: 'Massa tradicional com canéla, cravo e mel, recheio de doce de leite.',
         img: 'assets/images/shop/cookie08.jpg'
     },
+];
+
+
+brigadeiros = [
+    {
+        id: 1,
+        price: 2.50,
+        name: 'Brigadeiro',
+        description: 'Brigadeiro de paçoca.',
+        img: 'assets/images/shop/brigadeiro02.jpg'
+    },
+    {
+        id: 2,
+        price: 2.50,
+        name: 'Brigadeiro',
+        description: 'Brigadeiro de limão.',
+        img: 'assets/images/shop/brigadeiro03.jpg'
+    },
+    {
+        id: 3,
+        price: 2.50,
+        name: 'Brigadeiro',
+        description: 'Brigadeiro de ninho com nutela.',
+        img: 'assets/images/shop/brigadeiro04.jpg'
+    },
+    {
+        id: 4,
+        price: 2.50,
+        name: 'Brigadeiro',
+        description: 'Brigadeiro de coco.',
+        img: 'assets/images/shop/brigadeiro05.jpg'
+    },
+    {
+        id: 5,
+        price: 2.50,
+        name: 'Brigadeiro',
+        description: 'Brigadeiro ao leite.',
+        img: 'assets/images/shop/brigadeiro08.jpg'
+    },
+    {
+        id: 6,
+        price: 5.00,
+        name: 'Brigadeiro Recheado',
+        description: 'Bombom com morango.',
+        img: 'assets/images/shop/brigadeiro07.jpg'
+    },
+    {
+        id: 7,
+        price: 4.00,
+        name: 'Brigadeiros Recheado',
+        description: 'Brigadeiro com morango.',
+        img: 'assets/images/shop/brigadeiro01.jpg'
+    },
+
+    {
+        id: 8,
+        price: 6.00,
+        name: 'Brigadeiro Recheado',
+        description: 'Brigadeiro com bolo de cenoura.',
+        img: 'assets/images/shop/brigadeiro06.jpg'
+    },
+];
+
+brownies = [
+    {
+        id: 7,
+        price: 5.00,
+        name: 'Brownie Simples',
+        description: 'Massa tradicional, sem cobertura.',
+        img: 'assets/images/shop/brownie01.jpg'
+    },
+    {
+        id: 7,
+        price: 8.00,
+        name: 'Brownie Comfeitado',
+        description: 'Com brigadeiros ou morangos',
+        img: 'assets/images/shop/brownie02.jpg'
+    },
 ]
+
+combos = [
+    {
+        id: 1,
+        price: 7.00,
+        name: 'combos',
+        description: 'Massa tradicional com canéla, cravo e mel, recheio de doce de leite.',
+        img: 'assets/images/shop/combos/comboBri.jpg'
+    },
+    {
+        id: 2,
+        price: 7.00,
+        name: 'combos',
+        description: 'Massa tradicional com canéla, cravo e mel, recheio de doce de leite.',
+        img: 'assets/images/shop/combos/comboBri02.jpg'
+    },
+    {
+        id: 3,
+        price: 7.00,
+        name: 'combos',
+        description: 'Massa tradicional com canéla, cravo e mel, recheio de doce de leite.',
+        img: 'assets/images/shop/combos/comboBri03.jpg'
+    },
+];
+
+
+boloDePote = [
+    {
+        id: 1,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de cenoura com brigadeiro',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 2,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de chocolate com morango.',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 3,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de baunilha com morango.',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 4,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de abacaxi com morango',
+        img: 'assets/images/shop/combos/'
+    },
+];
+
+
+dataComemorativa = [
+    {
+        id: 1,
+        price: 10.00,
+        name: 'Combo Festa junina.',
+        description: 'Bolo de cenoura com brigadeiro',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 2,
+        price: 10.00,
+        name: 'Ovo de páscoa',
+        description: 'Bolo de chocolate com morango.',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 3,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de baunilha com morango.',
+        img: 'assets/images/shop/combos/'
+    },
+    {
+        id: 4,
+        price: 10.00,
+        name: 'Bolo de Pote',
+        description: 'Bolo de abacaxi com morango',
+        img: 'assets/images/shop/combos/'
+    },
+];
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -27,11 +27,33 @@ function start() {
 window.addEventListener("load", start)
 
 
+
+const menuTabs = document.querySelector(".links-shop");
+
+menuTabs.addEventListener("click", function (e) {
+    if (e.target.classList.contains("menu-tab-item") && !e.target.classList.contains("active")) {
+        //mudar cor das tabs
+        const target = e.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("tabColor", "active");
+        e.target.classList.add("tabColor", "active");
+
+        console.log(target)
+
+        //mudar div
+        const main = document.querySelector(".shop-container");
+        main.querySelector(".tab-content.active").classList.remove("active");
+        main.querySelector(target).classList.add("active");
+
+        console.log(main)
+    }
+});
+
+
 cookies.map((item, index) => {
     // console.log(item)
     let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
     console.log(shopItem)
-    document.querySelector('.shop-container').append(shopItem)
+    document.querySelector('.shop-container-cookie').append(shopItem)
 
     // preencher os dados de cada pizza
     shopItem.querySelector('.shop-item--img img').src = item.img
@@ -40,3 +62,77 @@ cookies.map((item, index) => {
     shopItem.querySelector('.shop-item--desc').innerHTML = item.description
 
 })
+
+brigadeiros.map((item, index) => {
+    // console.log(item)
+    let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
+    console.log(shopItem)
+    document.querySelector('.shop-container-brigadeiro').append(shopItem)
+
+    // preencher os dados de cada pizza
+    shopItem.querySelector('.shop-item--img img').src = item.img
+    shopItem.querySelector('.shop-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    shopItem.querySelector('.shop-item--name').innerHTML = item.name
+    shopItem.querySelector('.shop-item--desc').innerHTML = item.description
+
+})
+
+brownies.map((item, index) => {
+    // console.log(item)
+    let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
+    console.log(shopItem)
+    document.querySelector('.shop-container-brownie').append(shopItem)
+
+    // preencher os dados de cada pizza
+    shopItem.querySelector('.shop-item--img img').src = item.img
+    shopItem.querySelector('.shop-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    shopItem.querySelector('.shop-item--name').innerHTML = item.name
+    shopItem.querySelector('.shop-item--desc').innerHTML = item.description
+
+})
+
+combos.map((item, index) => {
+    // console.log(item)
+    let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
+    console.log(shopItem)
+    document.querySelector('.shop-container-combos').append(shopItem)
+
+    // preencher os dados de cada pizza
+    shopItem.querySelector('.shop-item--img img').src = item.img
+    shopItem.querySelector('.shop-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    shopItem.querySelector('.shop-item--name').innerHTML = item.name
+    shopItem.querySelector('.shop-item--desc').innerHTML = item.description
+
+});
+
+boloDePote.map((item, index) => {
+    // console.log(item)
+    let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
+    console.log(shopItem)
+    document.querySelector('.shop-container-boloDePote').append(shopItem)
+
+    // preencher os dados de cada pizza
+    shopItem.querySelector('.shop-item--img img').src = item.img
+    shopItem.querySelector('.shop-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+    shopItem.querySelector('.shop-item--name').innerHTML = item.name
+    shopItem.querySelector('.shop-item--desc').innerHTML = item.description
+
+});
+
+// dataComemorativa.map((item, index) => {
+    
+//     let shopItem = document.querySelector('.shop-content .shop-item').cloneNode(true);
+//     console.log(shopItem)
+//     document.querySelector('.shop-container-dataComemorativa').append(shopItem)
+
+    
+//     shopItem.querySelector('.shop-item--img img').src = item.img
+//     shopItem.querySelector('.shop-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
+//     shopItem.querySelector('.shop-item--name').innerHTML = item.name
+//     shopItem.querySelector('.shop-item--desc').innerHTML = item.description
+
+// });
+
+
+
+
